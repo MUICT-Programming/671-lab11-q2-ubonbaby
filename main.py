e279e23f80a50ba18955e4a35b3490b750eaa89f
Piriya Tharatum <ubonbaby@gmail.com>
2024-10-20 23:33:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# YOUR CODE HERE
+n=int(input())
+list1 = []
+list2 = []
+for i in range(n):
+        user_input = int(input())
+        list1.append(user_input)
+for i in range(n):
+        user_input = int(input())
+        list2.append(user_input)
+summarize = []
+def summation(n):
+  for i in range(n):
+      summarize.append(list1[i]+list2[i])
+  return summarize
+def find_min_max():
+      min= summarize[0]
+      max= summarize[0]
+      for i in range(len(summarize)):
+            if summarize[i] > max:
+                  max = summarize[i] 
+            if summarize[i] < min:
+                  min = summarize[i] 
+      return min,max
+print(summation(n))
+print(find_min_max())
